2374ccd5d71e35499496fe10849bd2468859fa52
ganeshsuryawansh <96677595+ganeshsuryawansh@users.noreply.github.com>
2022-09-22 13:09:38 +0530

    Add files via upload

---
+++ G_02_Nested_circle.py
@@ -0,0 +1,13 @@
+
+import turtle as tur
+
+tur.Turtle()
+tur.speed(0)
+tur.color("orange")
+tur.bgcolor("black")
+for j in range(1,70):
+    for i in range(1,6):
+        tur.left(144)
+        tur.forward(200)
+    tur.left(5)
+tur.done()
