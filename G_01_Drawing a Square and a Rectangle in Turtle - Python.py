018d17ad26c03eb40e40f18a1077729da68d2ce2
ganeshsuryawansh <96677595+ganeshsuryawansh@users.noreply.github.com>
2022-09-22 13:15:54 +0530

    Add files via upload

---
+++ G_01_Drawing a Square and a Rectangle in Turtle - Python.py
@@ -0,0 +1,13 @@
+import turtle
+
+ttl = turtle.Turtle()
+
+ttl.forward(150)  # moving the turtle Forward by 150 units
+ttl.left(90)  # Turning the turtle by 90 degrees
+ttl.forward(150)
+ttl.left(90)
+ttl.forward(150)
+ttl.left(90)
+ttl.forward(150)
+ttl.left(90)
+
